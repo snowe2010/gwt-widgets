1b30c76c83ec6ddf9d0144b886b8a9443c7aa6c4
snowe2010 <tylerbthrailkill@gmail.com>
2013-07-22 17:12:38 -0600

    Converted the gxt-basic tree to a gwt-tree
    Still needs work

--- lib/gxt-widgets/gxt_basic_tree.rb
+++ lib/gxt-widgets/gxt_basic_tree.rb
@@ -7,7 +7,7 @@ module GxtWidgets
     PageObject.register_widget :gxt_basic_tree, GxtBasicTree, 'div'
 
     def child_nodes
-      self.gxt_basic_tree_elements(:xpath=>child_xpath)
+      self.gxt_basic_tree_elements(:xpath=>child_node_xpath)
     end
 
     def name
@@ -22,39 +22,47 @@ module GxtWidgets
     end
 
     def joint
-      node_contents.image_element(:xpath=>".//img[contains(@class,'x-tree3-node-joint')]")
+      # node_contents.image_element(:xpath=>".//*[ancestor::tr//img[1]]")
+      node_contents.image_element(:xpath=> "ancestor-or-self::tr[1]//img[1]")
     end
 
     def expand
       joint.click unless self.expanded?
+      # joint.click
     end
 
     def expanded?
        self.node_container.visible?
     end
 
-
     def collapse
       joint.click unless self.collapsed?
+      # joint.click
     end
 
     def collapsed?
       !self.node_container.visible?
     end
 
-
-
+    #this is correct
     def node_contents
-      self.div_element(:xpath=>".//div[contains(@class,'x-tree3-el')]")
+      # self.div_element(:xpath=>".//div[contains(@class,'x-tree3-el')]")
+      self.div_element(:xpath=>".//div[contains(@class,'gwt-TreeItem')]")
     end
 
     def node_container
-      self.div_element(:xpath=>".//div[contains(@class,'x-tree3-node-ct')]")
+      # self.div_element(:xpath=>".//div[contains(@class,'x-tree3-node-ct')]")
+      self.div_element(:xpath=>".//table/following::div[1]")
     end
 
     @protected
-    def child_xpath
+    #this is correct?
+    def child_node_xpath
       ".//child::div[contains(@class,'x-tree3-node')]"
+    end
+
+    def child_leaf_xpath
+      ".//child::div[containts(@class, 'x-tree3-el')]"
     end
   end
 end
